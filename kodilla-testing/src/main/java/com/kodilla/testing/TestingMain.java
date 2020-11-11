418fcbc777576d72361126b6d23f774def932027
Maikody <micborla@gmail.com>
2020-11-11 19:40:59 +0100

    6.2 Added kodilla.testing.user and kodilla.testing.calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,37 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        System.out.println("Test user - pierwszy test jednostkowy:");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser"))
+            System.out.println("test OK");
+        else
+            System.out.println("Error!");
+
+        System.out.println("Test calculator_add - drugi test jednostkowy:");
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+
+        if (sum == Calculator.add(a,b))
+            System.out.println("test OK");
+        else
+            System.out.println("Error!");
+
+        System.out.println("Test calculator_substract - trzeci test jednostkowy:");        int substract = a - b;
+        int subtract = a - b;
+        if (subtract == Calculator.subtract(a,b))
+            System.out.println("test OK");
+        else
+            System.out.println("Error!");
+
+
     }
 }
