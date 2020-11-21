f5f4ffd705ed1237dae0ebc90e25017c746e8218
Maikody <73773432+Maikody@users.noreply.github.com>
2020-11-21 10:24:18 +0100

    7.5 Corrected testGetAverage() in ArrayOperationsTestSuite.java

--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -2,32 +2,15 @@ package com.kodilla.stream.array;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Random;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ArrayOperationsTestSuite {
-    public static double expectedAverage = 0;
 
     @Test
     public void testGetAverage(){
-        int[] input = arrayGenerator();
+        int[] input = {0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1};
 
-        assertEquals(expectedAverage, ArrayOperations.getAverage(input));
+        assertEquals(0.5, ArrayOperations.getAverage(input));
     }
 
-    public int[] arrayGenerator(){
-        int[] array = new int[20];
-        int sum = 0;
-
-        Random random = new Random();
-        for(int i = 0; i < 20; i++) {
-            array[i] = random.nextInt(10);
-            sum += array[i];
-        }
-
-        expectedAverage = (double) sum / 20;
-
-        return array;
-    }
 }
