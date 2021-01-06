cbcea3a9eec7b039fcdc62d97b0d3aedffcaf5cb
Maikody <73773432+Maikody@users.noreply.github.com>
2021-01-06 14:27:02 +0100

    13.1 Good-patterns module added + 1st challenge

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Main.java
@@ -0,0 +1,10 @@
+package com.kodilla.good.patterns.challenges;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MovieStore ms = new MovieStore();
+        ms.printMovies(ms.getMovies());
+    }
+
+}
