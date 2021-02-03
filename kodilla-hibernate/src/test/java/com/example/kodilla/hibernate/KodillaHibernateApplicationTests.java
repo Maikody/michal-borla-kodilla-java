ffdcb24d59f77c4a7332e6e54ec4e19267981fad
Maikody <73773432+Maikody@users.noreply.github.com>
2021-02-03 19:29:00 +0100

    17.1 Chapter added

---
+++ kodilla-hibernate/src/test/java/com/example/kodilla/hibernate/KodillaHibernateApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.kodilla.hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaHibernateApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
